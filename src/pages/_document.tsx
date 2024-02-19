c70744c8370606611e3529d309601bc2a6464639
Gun <footmenkingdom@gmail.com>
2024-02-19 09:52:07 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
-import { GA_TRACKING_ID } from '@/features/analytics'
+//import { GA_TRACKING_ID } from '@/features/analytics'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -32,19 +32,6 @@ class MyDocument extends Document {
             content="Sightread demo displaying falling notes visualization"
           />
 
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });`,
-            }}
-          />
         </Head>
         <body>
           <Main />
