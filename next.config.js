d7542f1f9ae33332339be124c42a2f99543db755
Gun <footmenkingdom@gmail.com>
2024-03-12 16:12:21 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
+  config: {
+    optimization: {
+      minimizer: [],
+      minimize: false,
+      minify: false,
+      splitChunks: {
+          chunks: 'all',
+          name: true
+      },
+      runtimeChunk: true
+    },
+  }
   compress: false,
 }
